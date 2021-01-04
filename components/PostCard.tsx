a9d0092ae3e37dc71dce6e4bd994e3c58287f405
harukitosa <13haruki28@gmail.com>
2021-01-04 20:14:04 +0900

    Develop:create home grid layout

---
+++ components/PostCard.tsx
@@ -0,0 +1,44 @@
+import React, {FC} from 'react'
+
+interface Props {
+    imgURL: string,
+}
+
+const PostCard: FC<Props> = ({imgURL}) => {
+    return (
+        <div className="w-full grid-layout">
+            <div className="bg-white rounded-lg p-2 m-1">
+                    <div>
+                        <img src={imgURL} alt="macbook" />
+                    </div>
+                    <div className="pt-2 flex flex-row items-center">
+                        <img className="rounded-full" src="./icon.jpg" width="30" alt=""/>
+                        <div className="pl-2 text-sm">
+                            <p>とさ</p>
+                        </div>
+                    </div>
+                    <div className="text-sm pt-2">
+                        お金が貯まったので購入しました！
+                        スペックが非常に高くて良かったです！...
+                    </div>
+                    <div className="pt-4 flex flex-row justify-between">
+                        <div className="flex flex-wrap">
+                            <div className="flex flex-row text-sm items-center pl-2">
+                                <img src="./heart-outline.svg" alt="heart" width="20" />
+                                <p className="pl-1">12</p>
+                            </div>
+                            <div className="flex flex-row text-sm items-center pl-2">
+                                <img src="./chatbox-outline.svg" alt="heart" width="20" />
+                                <p className="pl-1">0</p>
+                            </div>
+                        </div>
+                        <div className="text-sm pr-2 text-gray-300">
+                            2020-12
+                        </div>
+                </div>
+            </div>
+        </div>
+    );
+} 
+
+export default PostCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, {FC, useContext} from 'react'
 import Head from 'next/head'
-import { AuthContext } from '../src/context/Auth';
+import { AuthContext } from '../src/context/Auth'
+import PostCard from '../components/PostCard'
 // import SignInWithTwitter from '../components/SignInWithTwitter'
 
 const Home: FC = () => {
@@ -11,7 +12,14 @@ const Home: FC = () => {
                 <title>Create Next App</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="main h-screen bg-gray-100">
+            <div className="main bg-gray-100 grid-container">
+                <PostCard imgURL="./mbp-silver-gallery1-202011.jpeg"/>
+                <PostCard imgURL="./mbp13-witb-silver-202005.jpeg"/>
+                <PostCard imgURL="./mbp-silver-gallery1-202011.jpeg"/>
+                <PostCard imgURL="./mbp-silver-gallery1-202011.jpeg"/>
+                <PostCard imgURL="./mbp13-witb-silver-202005.jpeg"/>
+                <PostCard imgURL="./mbp-silver-gallery1-202011.jpeg"/>
+                <PostCard imgURL="./mbp-silver-gallery1-202011.jpeg"/>
                 {/* { currentUser !== undefined &&
                     <SignInWithTwitter>
                         twitterでログインする
