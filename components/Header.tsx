c96004198d4b5873da4f5dca63a715b571e54514
harukitosa <13haruki28@gmail.com>
2021-01-04 14:58:56 +0900

    Fix:Headerの作成

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react'
+
+const Header: FC = () => {
+    return (
+        <div className="main bg-gray-300 h-12">
+            Header
+        </div>
+    );
+} 
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { AuthProvider } from '../src/context/Auth'
+import Header from '../components/Header'
 import '../styles/globals.css'
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
       <div>
+        <Header/>
         <AuthProvider>
             <Component {...pageProps} />
         </AuthProvider>
